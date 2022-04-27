b1ad0110b2a11fe68d2fd481fdf2100953b81c41
kjohann <kristian.johannessen@bekk.no>
2022-04-27 10:30:46 +0200

    Firebase init

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { initializeApp } from "firebase/app";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+initializeApp({
+  apiKey: "AIzaSyDGBvh3MyrUf6xg-hfZBXjYyKl11ujgmpA",
+  authDomain: "dinnrplanr.firebaseapp.com",
+  projectId: "dinnrplanr",
+  storageBucket: "dinnrplanr.appspot.com",
+  messagingSenderId: "1084507204220",
+  appId: "1:1084507204220:web:f12ac8819469e62cea37f9"
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,8 +21,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
